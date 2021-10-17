5053200036399331231e1666582450cc6eb6fd81
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2021-10-17 15:19:59 +0200

    main container

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import TodoList from "./components/todoList";
 import { Route, Switch } from "react-router-dom";
 import EditTodo from "./components/edit/edit";
 import PropTypes from "prop-types";
+import { Container } from "./appStyle.js";
 
 function App({ location }) {
   return (
-    <div className="App">
+    <Container>
       <Switch location={location}>
         <Route exact path="/home" component={TodoList} />
         <Route path="/edit/:id" component={EditTodo} />
         <Route component={TodoList} />
       </Switch>
-    </div>
+    </Container>
   );
 }
 App.propTypes = {
---
+++ src/appStyle.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+export const Container = styled.main`
+  max-width: 400px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
